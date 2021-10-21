aa221adcfe231f3d4066afcebc4bf7c22184d45b
k-basinski <k.basinski@gumed.edu.pl>
2021-10-21 12:25:10 +0200

    w04 i 05

--- make_slides.sh
+++ make_slides.sh
@@ -4,6 +4,8 @@
 marp 01_intro.md -o 01_intro.html
 marp 02_evo.md -o 02_evo.html
 marp 03_neuro.md -o 03_neuro.html
+marp 04_perception.md -o 04_perception.html
+marp 05_mem_iq.md -o 05_mem_iq.html
 
 # pdfs
 marp 01_intro.md -o pdf/01_intro.pdf --allow-local-files
